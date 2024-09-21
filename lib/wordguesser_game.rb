516db352c2ef66e7613f657a417f124b08cbb108
Milana Gurbanova <milana.gurbanova@cdtm.de>
2024-09-21 21:51:05 +0000

    Ready for Herokugit init

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -3,10 +3,15 @@ class WordGuesserGame
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/wordguesser_game_spec.rb pass.
 
-  # Get a word from remote "random word" service
+  attr_accessor :word, :guesses, :wrong_guesses
+  # dies generiert automatisch getter und setter für die attribute
 
+  # Get a word from remote "random word" service
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+
   end
 
   # You can test it by installing irb via $ gem install irb
@@ -22,4 +27,51 @@ class WordGuesserGame
     }
   end
 
+  def guess (letter)
+    # ist letter ein letter?
+    raise ArgumentError if letter.nil? || letter == '' || !letter.match?(/[a-zA-Z]/)
+
+    letter = letter.downcase #to make it case-insensitive
+
+    # Wenn Buchstabe bereits geraten, false zurückgeben
+    return false if @guesses.include?(letter) || @wrong_guesses.include?(letter)
+
+    # neuer Buchstabe wird geraten
+    if @word.include?(letter)
+      # correct letter guessed
+      @guesses += letter unless @guesses.include?(letter)
+      return true  
+    else
+      # incorrect letter guessed
+      @wrong_guesses += letter unless @wrong_guesses.include?(letter)
+      return true  
+    end
+  end  
+
+  def word_with_guesses
+    displayed_word = '' 
+
+    @word.each_char do |letter|  # buchstaben des wortes durchgehen
+      if @guesses.include?(letter) #check ob geraten
+        displayed_word += letter 
+      else
+        displayed_word += '-' # wenn nicht geraten
+      end
+    end
+
+    displayed_word # Rückgabe der angeordneten Zeichenkette
+  end
+
+  def check_win_or_lose
+    # alle eraten?
+    if @word.chars.all? { |letter| @guesses.include?(letter) }
+      return :win
+    # alle 7 guesses falsch?
+    elsif @wrong_guesses.length >= 7
+      return :lose
+    # Wenn weder gewonnen noch verloren, wird weitergespielt
+    else
+      return :play
+    end
+  end
 end
